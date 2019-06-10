2d71318136612f8873c9528f1b1e29a997bba935
msanchez <mafecharm@gmail.com>
2019-06-10 16:09:16 -0500

    codigo comentado

--- src/main/java/com/test/spring/web/PersonaController.java
+++ src/main/java/com/test/spring/web/PersonaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -18,11 +19,11 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
-    @RequestMapping(value = "/find-all.do")
+    @RequestMapping(value = "/find-all.do", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<Persona>> findAll() {
 
-        List<Persona> personaList =personaService.findAll();
+        List<Persona> personaList = personaService.findAll();
 
         return new ResponseEntity<>(personaList, HttpStatus.OK);
     }
